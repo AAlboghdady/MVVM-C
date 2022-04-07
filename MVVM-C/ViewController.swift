cd9a7b30b7bc3f66ac98738c99216218532112c4
Abdurrahman Alboghdady <abdurrahman.alboghdady@gmail.com>
2022-04-08 00:42:59 +0200

    Initial Commit

---
+++ MVVM-C/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MVVM-C
+//
+//  Created by Abdurrahman Alboghdady on 08/04/2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
