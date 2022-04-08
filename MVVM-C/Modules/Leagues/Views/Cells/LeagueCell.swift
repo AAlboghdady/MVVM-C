0a4cc174ff1e426147ca8f419cd6544d4e670ed9
Abdurrahman Alboghdady <abdurrahman.alboghdady@gmail.com>
2022-04-08 02:41:25 +0200

    adding leagues module with league view

---
+++ MVVM-C/Modules/Leagues/Views/Cells/LeagueCell.swift
@@ -0,0 +1,22 @@
+//
+//  LeagueCell.swift
+//  MVVM-C
+//
+//  Created by Abdurrahman Alboghdady on 08/04/2022.
+//
+
+import UIKit
+
+class LeagueCell: UITableViewCell {
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+}
